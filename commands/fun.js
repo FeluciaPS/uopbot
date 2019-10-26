eacf91f60989e58c63d39ab4b52aae2c862f0005
TheMezStrikes <=>
2019-10-26 13:32:47 +0200

    fixed things

--- commands/fun.js
+++ commands/fun.js
@@ -7,22 +7,19 @@ module.exports = {
 		on: function(room, user, args) {
 			if (!user.can(room, '#')) return;
 			room.settings.hellothere = true;
-			room.saveSettings();
-			room.loadSettings();
+			room.saveSettings(true);
 			room.send('Hello There auto response enabled');
 		},
 		off: function(room, user, args) {
 			if (!user.can(room, '#')) return;
 			room.settings.hellothere = false;
-			room.saveSettings();
-			room.loadSettings();
+			room.saveSettings(true);
 			room.send('Hello There auto response disabled');
 		},
 		toggle: function(room, user, args) {
 			if (!user.can(room, '#')) return;
 			room.settings.hellothere = !room.settings.hellothere;
-			room.saveSettings();
-			room.loadSettings();
+			room.saveSettings(true);
 			room.send(`Hello There auto response ${room.settings.hellothere ? 'en' : 'dis'}abled`);
 		},
 	}
