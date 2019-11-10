64b296c01d53559a43d949c4da45fed8d1a24f22
TheMezStrikes <=>
2019-11-10 20:02:19 +0100

    I'm dumb

--- commands/monotype.js
+++ commands/monotype.js
@@ -12,7 +12,7 @@ let canMakeTour = function(room, user) {
 }
 
 let checkGenerator = function(room, meta, args) {
-    if (args) {
+    if (args && args[0]) {
         if (args[0].startsWith("rr")) {
             let count = parseInt(args[0].substring(2));
             if (count) room.send(`/tour create ${meta}, rr,, ${count}`);
