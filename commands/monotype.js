3f791e2c1bba706f6be93003be51fc0a4feca4e0
TheMezStrikes <TheMezStrikes@gmail.com>
2020-02-10 23:13:46 +0100

    Update monotype.js

--- commands/monotype.js
+++ commands/monotype.js
@@ -124,7 +124,8 @@ module.exports = {
 		let format = BLT.getNext();
 		BLT.last = format;
 		BLT.next = false;
-		room.send(`/tour create ${format}monotype, elim`)
+		room.send(`/tour create ${format}monotype, elim`);
+		room.send('/tour scouting disallow');
 		room.startTour('blt');
 	},
 	nextblt: function(room, user, args) {
