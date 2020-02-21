06c3092c23d249a59011cb8f9ae4512f1816ea5f
TheMezStrikes <TheMezStrikes@gmail.com>
2020-02-22 00:46:25 +0100

    autohide command

--- commands/fun.js
+++ commands/fun.js
@@ -22,5 +22,26 @@ module.exports = {
 			room.saveSettings(true);
 			room.send(`Hello There auto response ${room.settings.hellothere ? 'en' : 'dis'}abled`);
 		},
+	},
+        autohide: {
+		'': 'on',
+		on: function(room, user, args) {
+			if (!user.can(room, '#')) return;
+			room.settings.autohide = true;
+			room.saveSettings(true);
+			room.send('Automatic hidetext for mutes enabled');
+		},
+		off: function(room, user, args) {
+			if (!user.can(room, '#')) return;
+			room.settings.autohide = false;
+			room.saveSettings(true);
+			room.send('Automatic hidetext for mutes disabled');
+		},
+		toggle: function(room, user, args) {
+			if (!user.can(room, '#')) return;
+			room.settings.autohide = !room.settings.autohide;
+			room.saveSettings(true);
+			room.send(`Automatic hidetext  for mutes ${room.settings.autohide ? 'en' : 'dis'}abled`);
+		},
 	}
 }
