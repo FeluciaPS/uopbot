49ed30c74afe5befc2e6a105fb956062489ea2ea
TheMezStrikes <TheMezStrikes@gmail.com>
2020-04-27 23:23:56 +0200

    Update parser.js

--- parser.js
+++ parser.js
@@ -177,8 +177,8 @@ bot.on('tournament', (parts, data) => {
             if (!Rooms['1v1']) return;
             let format = Tournament.formats[toId(parts[3])] ? Tournament.formats[toId(parts[3])] : parts[3]
             if (room.id === "1v1oldgens") Rooms['1v1'].send(`${format.replace('OU', '1v1')} tournament in <<1v1og>>`);
-            if (room.id === "tournaments" && parts[3].match(/\dv\d/)) Rooms['1v1'].send(`${format} tournament in <<tours>>`);
-            if (room.id === "toursplaza" && parts[3].match(/\dv\d/)) Rooms['1v1'].send(`${format} tournament in <<tp>>`);
+            if (room.id === "tournaments" && parts[3].match(/\dv\d/)) Rooms[parts[3].match(/\dv\d/)[0]].send(`${format} tournament in <<tours>>`);
+            if (room.id === "toursplaza" && parts[3].match(/\dv\d/)) Rooms[parts[3].match(/\dv\d/)[0]].send(`${format} tournament in <<tp>>`);
             if (room.id === "tournaments" && parts[3].indexOf('nfe') !== -1) Rooms['nfe'].send(`${format} tournament in <<tours>>`);
             if (room.id === "toursplaza" && parts[3].indexOf('nfe') !== -1) Rooms['nfe'].send(`${format} tournament in <<tp>>`);
         }
