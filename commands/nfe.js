c72b7aa7481e0ed170ddac3c978d57e947a0dabc
TheMezStrikes <TheMezStrikes@gmail.com>
2020-05-31 23:13:13 +0200

    Update nfe.js

--- commands/nfe.js
+++ commands/nfe.js
@@ -25,37 +25,6 @@ global.NFE = {
     }
 }
 
-global.NatDex = { // this doesn't belong here but who cares
-    times: [ 0, 20, 21, 22, 23 ],
-    formats: [ 'uu', 'uu', 'gen8', 'gen8', 'gen8' ],
-    last: parseInt(require('fs').readFileSync("./data/lastnatdex.txt")),
-    official: function() {
-        let room = Rooms['nationaldex'];
-        let now = new Date(Date.now());
-        if (this.last === -1) return;
-        let next = (this.last + 1) % this.times.length;
-        let mins = now.getMinutes();
-        if (mins > 10) return;
-        let hours = now.getHours();
-        if (hours === this.times[next]) {
-            if (room.tournament) {
-                if (room.tournament.official) return;
-                else {
-                    room.send("/wall Official time. Ending ongoing tournament");
-                    room.send("/tour end");
-                    room.endTour();
-                }
-            }
-            require('fs').writeFileSync("./data/lastnatdex.txt", next);
-            this.last = next;
-            console.log("natdex official")
-            let args = ["o"];
-            if (next < 2) args = []
-            Commands['natdex'][this.formats[next]](room, Users.staff, args);
-        }
-    }
-}
-
 let canMakeTour = function(room, user) {
     // I'm gonna use this a lot so why not make a function for it
     if (room != 'nfe') return false;
@@ -89,26 +58,6 @@ let checkGenerator = function(room, meta, args, tourname = '') {
 }
 
 module.exports = {
-    natdex: {
-        '': 'gen8',
-        gen8: function(room, user, args) {
-            if (!user.can(room, "%")) return false;
-            if (room.tournament) {
-                room.send("A tournament is already going on.");
-                return false;
-            }
-            checkGenerator(room, 'gen8nationaldex', args);
-        },
-        uu: function(room, user, args) {
-            if (!user.can(room, "%")) return false;
-            if (room.tournament) {
-                room.send("A tournament is already going on.");
-                return false;
-            }
-            checkGenerator(room, 'gen8nationaldex', args);
-            room.send('/tour rules -Landorus-Therian, -Darmanitan-Galar, -Greninja-Ash, -Heatran, -Dracovish, -Dragapult, -Ferrothorn, -Kartana, -Metagross-Mega, -Volcarona, -Tapu Fini, -Chansey, -Clefable, -Magearna, -Tapu Koko, -Garchomp, -Excadrill, -Toxapex, -Zapdos, -Tapu Lele, -Corviknight, -Tangrowth, -Tornadus-Therian, -Pelipper, -Lopunny-Mega, -Scizor-Mega, -Gliscor, -Greninja, -Melmetal, -Serperior, -Magnezone, -Swampert-Mega, -Bisharp, -Blacephalon, -Tapu Bulu, -Rotom-Heat, -Ditto, -Tyranitar, -Grimmsnarl, -Rotom-Wash, -Aegislash, -Alakazam-Mega, -Charizard-Mega-X, -Charizard-Mega-Y, -Deoxys-Defense, -Dragonite, -Gallade-Mega, -Hawlucha, -Heracross-Mega, -Hoopa-Unbound, -Latias-Mega, -Latios, -Latios-Mega, -Mawile-Mega, -Medicham-Mega, -Mew, -Pinsir-Mega, -Staraptor, -Thundurus-Base, -Victini, -Drizzle, -Drought, -Aurora Veil, -Azumarill');
-        }
-    },
     nfe: {
         '': 'gen8',
         gen8: function(room, user, args) {
