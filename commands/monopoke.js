bcbfdcad9a2bf39a81f98f6856fa5f52fba5440f
TheMezStrikes <TheMezStrikes@gmail.com>
2020-06-05 15:04:48 +0200

    Update monopoke.js

--- commands/monopoke.js
+++ commands/monopoke.js
@@ -11,38 +11,102 @@ let canMakeTour = function(room, user) {
     return true;
 }
 
-let chooseMonopoke = function() {
-    let mons = Object.keys(t);
+let chooseMonopoke = function(gen) {
+    let mons = [];
+    for (let i in PokeDex) {
+        if (PokeDex[i].baseSpecies) continue;
+        if (gen === "gen7" && PokeDex[i].num > 809) continue;
+        if (gen === "gen8" && fdata[i].isNonstandard === "Past") continue;
+        mons.push(i);
+    }
     let mon = Math.floor(Math.random() * mons.length);
     mon = mons[mon];
-    mon = t[mon];
-    if (mon.species.indexOf("-") != -1) mon = chooseMonopoke();
+    mon = PokeDex[mon];
+    if (mon.baseSpecies) mon = chooseMonopoke(gen);
     return mon.species;
 }
 
 module.exports = {
-    monopoke: function(room, user, args) {
-        if (!canMakeTour(room, user)) return;
-        if (!args[0]) args[0] = chooseMonopoke();
-        if (!t[toId(args[0])]) return room.send(`${args[0]} is not a Pokémon`);
-        let mon = t[toId(args[0])].species;
-        let ruleset = "/tour rules !Team Preview, -" + Banlist['1v1-allowed'].join(', -') + `, +${mon}`;
-        Commands['1v1']['gen7'](room, user, args);
-        room.startTour("monopoke");
-        room.send(ruleset);
-        room.send("/tour name Monopoke " + mon);
-        room.send(`/wall Monopoke ${mon}! Use only ${mon}`);
+    monopoke: {
+        '': function(room, user, args) {
+            if (!canMakeTour(room, user)) return;
+            if (!args[0]) args[0] = chooseMonopoke(false);
+            let dex = PokeDex[toId(args[0])];
+            let fdt = fdata[toId(args[0])];
+            if (!dex) return room.send(`${args[0]} is not a Pokémon`);
+            if (fdt.isNonstandard === "Past") return Commands['monopoke']['gen7'](room, user, args);
+            else return Commands['monopoke']['gen8'](room, user, args);
+        },
+        gen8: function(room, user, args) {
+            if (!canMakeTour(room, user)) return;
+            if (!args[0]) args[0] = chooseMonopoke('gen8');
+            let dex = PokeDex[toId(args[0])];
+            let fdt = fdata[toId(args[0])];
+            if (!dex) return room.send(`${args[0]} is not a Pokémon`);
+            if (fdt.isNonstandard === "Past") return room.send(`${dex.species} is only available in past gens`);
+            let mon = dex.species;
+            let ruleset = "/tour rules !Team Preview, -All Pokemon, +${mon}`;
+            Commands['1v1']['gen8'](room, user, args);
+            room.startTour("monopoke");
+            room.send(ruleset);
+            room.send("/tour name [Gen 8] Monopoke " + mon);
+            room.send(`/wall Monopoke ${mon}! Use only ${mon}`);
+        },
+        gen7: function(room, user, args) {
+            if (!canMakeTour(room, user)) return;
+            if (!args[0]) args[0] = chooseMonopoke('gen7');
+            let dex = PokeDex[toId(args[0])];
+            let fdt = fdata[toId(args[0])];
+            if (!dex) return room.send(`${args[0]} is not a Pokémon`);
+            if (dex.num > 809) return room.send(`${dex.species} is not available in gen 7.`);
+            let mon = dex.species;
+            let ruleset = "/tour rules !Team Preview, -All Pokemon, +${mon}`;
+            Commands['1v1']['gen7'](room, user, args);
+            room.startTour("monopoke");
+            room.send(ruleset);
+            room.send("/tour name [Gen 7] Monopoke " + mon);
+            room.send(`/wall Monopoke ${mon}! Use only ${mon}`);
+        }
     },
-    camonopoke: function(room, user, args) {
-        if (!canMakeTour(room, user)) return;
-        if (!args[0]) args[0] = chooseMonopoke();
-        if (!t[toId(args[0])]) return room.send(`${args[0]} is not a Pokémon`);
-        let mon = t[toId(args[0])].species;
-        let ruleset = "/tour rules !Team Preview, [Gen 7] Camomons, -" + Banlist['1v1-allowed'].join(', -') + `, +${mon}`;
-        Commands['1v1']['gen7'](room, user, args);
-        room.startTour("monopoke");
-        room.send(ruleset);
-        room.send("/tour name Camonopoke " + mon);
-        room.send(`/wall Camomons Monopoke ${mon}! Use only ${mon}`);
+    camonopoke: {
+        '': function(room, user, args) {
+            if (!canMakeTour(room, user)) return;
+            if (!args[0]) args[0] = chooseMonopoke(false);
+            let dex = PokeDex[toId(args[0])];
+            let fdt = fdata[toId(args[0])];
+            if (!dex) return room.send(`${args[0]} is not a Pokémon`);
+            if (fdt.isNonstandard === "Past") return Commands['camonopoke']['gen7'](room, user, args);
+            else return Commands['camonopoke']['gen8'](room, user, args);
+        },
+        gen8: function(room, user, args) {
+            if (!canMakeTour(room, user)) return;
+            if (!args[0]) args[0] = chooseMonopoke('gen8');
+            let dex = PokeDex[toId(args[0])];
+            let fdt = fdata[toId(args[0])];
+            if (!dex) return room.send(`${args[0]} is not a Pokémon`);
+            if (fdt.isNonstandard === "Past") return room.send(`${dex.species} is only available in past gens`);
+            let mon = dex.species;
+            let ruleset = "/tour rules !Team Preview, -All Pokemon, +${mon}, [Gen 8] Camomons`;
+            Commands['1v1']['gen8'](room, user, args);
+            room.startTour("monopoke");
+            room.send(ruleset);
+            room.send("/tour name [Gen 8] Camonopoke " + mon);
+            room.send(`/wall Camomons Monopoke ${mon}! Use only ${mon}`);
+        },
+        gen7: function(room, user, args) {
+            if (!canMakeTour(room, user)) return;
+            if (!args[0]) args[0] = chooseMonopoke('gen7');
+            let dex = PokeDex[toId(args[0])];
+            let fdt = fdata[toId(args[0])];
+            if (!dex) return room.send(`${args[0]} is not a Pokémon`);
+            if (dex.num > 809) return room.send(`${dex.species} is not available in gen 7.`);
+            let mon = dex.species;
+            let ruleset = "/tour rules !Team Preview, -All Pokemon, +${mon}, [Gen 8] Camomons`;
+            Commands['1v1']['gen7'](room, user, args);
+            room.startTour("monopoke");
+            room.send(ruleset);
+            room.send("/tour name [Gen 7] Camonopoke " + mon);
+            room.send(`/wall Camomons Monopoke ${mon}! Use only ${mon}`);
+        }
     }
 }
