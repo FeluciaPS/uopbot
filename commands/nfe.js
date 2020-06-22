0eb648cff5831b6d13ba998cb5a89b7bebcdecb4
DrOnline-Tack <67210748+DrOnline-Tack@users.noreply.github.com>
2020-06-22 14:39:57 -0400

    Banlist update for oldgens (#21)

--- commands/nfe.js
+++ commands/nfe.js
@@ -73,7 +73,7 @@ module.exports = {
         gen6: function(room, user, args) {
             if (!canMakeTour(room, user)) return;
             checkGenerator(room, 'gen6ru', args, '[Gen 6] NFE');
-            room.send('/tour rules Not Fully Evolved, +UU, +OU, +UUBL, +RUBL, +NUBL, +PUBL, +PU, +Uber, -Chansey, -Doublade, -Fletchinder, -Gligar, -Golbat, -Gurdurr, -Haunter, -Machoke, -Magneton, -Pawniard, -Piloswine, -Porygon2, -Rhydon, -Servine, -Scyther, -Sneasel, -Vigoroth');
+            room.send('/tour rules Not Fully Evolved, +UU, +OU, +UUBL, +RUBL, +NUBL, +PUBL, +PU, +Uber, -Chansey, -Doublade, -Fletchinder, -Gligar, -Golbat, -Gurdurr, -Haunter, -Machoke, -Magneton, -Monferno, -Pawniard, -Piloswine, -Porygon2, -Rhydon, -Servine, -Scyther, -Sneasel, -Vigoroth');
         },
         gen5: function(room, user, args) {
             if (!canMakeTour(room, user)) return;
@@ -88,7 +88,7 @@ module.exports = {
         gen3: function(room, user, args) {
             if (!canMakeTour(room, user)) return;
             checkGenerator(room, 'gen3nu', args, '[Gen 3] NFE');
-            room.send('/tour rules Not Fully Evolved, -Diglett, -Dragonair, -Haunter, -Vigoroth, -Light Ball');
+            room.send('/tour rules Not Fully Evolved, -Abra, -Diglett, -Dragonair, -Golbat, -Haunter, -Metang, -Vigoroth, -Light Ball');
         },
         // OMs
         natdex: function(room, user, args) {
