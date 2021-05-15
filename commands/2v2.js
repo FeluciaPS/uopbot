c1ed6cf6308bd24a0db7581b3f969ffa5fc6e104
MubsPokesart <51163599+MubsPokesart@users.noreply.github.com>
2021-05-15 10:20:39 -0400

    Ruleset changes for 2v2.js

--- commands/2v2.js
+++ commands/2v2.js
@@ -1,4 +1,4 @@
-let moncountrule = 'Max Team Size = 4';
+let moncountrule = 'max team size = 4, min team size = 2, picked team size = 2';
 
 module.exports = {
     '2v2': {
@@ -10,25 +10,25 @@ module.exports = {
         gen7: function (room, user, args) {
             if (!Utils.canMakeTour(room, user)) return;
             Utils.checkGenerator(room, 'gen7doublesou', args, '[Gen 7] 2v2 Doubles');
-            room.send('/tour rules Two vs Two, Accuracy Moves Clause, Z-Move Clause, +Gengar-Mega, -Kangaskhan-Mega, -Salamence-Mega, -Final Gambit, -Focus Sash, -Tapu Lele, -Perish Song' + moncountrule);
+            room.send('/tour rules Accuracy Moves Clause, Z-Move Clause, +Gengar-Mega, -Kangaskhan-Mega, -Salamence-Mega, -Final Gambit, -Focus Sash, -Tapu Lele, +Snorlax, -Perish Song, ' + moncountrule);
         },
         oras: 'gen6',
         gen6: function (room, user, args) {
             if (!Utils.canMakeTour(room, user)) return;
             Utils.checkGenerator(room, 'gen6doublesou', args, '[Gen 6] 2v2 Doubles');
-            room.send('/tour rules Two vs Two, -Perish Song, -Focus Sash, -Kangaskhan-Mega' + moncountrule);
+            room.send('/tour rules Accuracy Moves Clause, -Perish Song, -Focus Sash, -Kangaskhan-Mega, ' + moncountrule);
         },
         bw: 'gen5',
         gen5: function (room, user, args) {
             if (!Utils.canMakeTour(room, user)) return;
             Utils.checkGenerator(room, 'gen5doublesou', args, '[Gen 5] 2v2 Doubles');
-            room.send('/tour rules Two vs Two, -Perish Song, -Focus Sash' + moncountrule);
+            room.send('/tour rules Accuracy Moves Clause, -Perish Song, -Focus Sash, -Kingdra, ' + moncountrule;
         },
         dp: 'gen4',
         gen4: function (room, user, args) {
             if (!Utils.canMakeTour(room, user)) return;
             Utils.checkGenerator(room, 'gen4doublesou', args, '[Gen 4] 2v2 Doubles');
-            room.send('/tour rules Two vs Two, -Perish Song, -Focus Sash' + moncountrule);
+            room.send('/tour rules Two vs Two, -Perish Song, -Focus Sash, ' + moncountrule);
         },
         inverse: function (room, user, args) {
             if (!Utils.canMakeTour(room, user)) return;
