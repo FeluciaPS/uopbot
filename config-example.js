a024eb55f47a28fe939554cc230d1f90aeede0c5
FeluciaPS <TheMezStrikes@gmail.com>
2023-03-20 01:08:30 +0100

    Update config-example.js

--- config-example.js
+++ config-example.js
@@ -11,9 +11,24 @@ exports.password = "";
 
 // Please use full room names, and not aliases. Some code relies on these arrays.
 exports.rooms = [];
+
+// Hidden rooms that shouldn't be leaked.
 exports.privaterooms = [...exports.rooms];
+
+// Command char
 exports.char = ".";
+
+// Devs are big important people be careful who you add here
 exports.devs = [];
+
+// Git
+exports.git = "";
+
+// I don't remember what this variable does but the bot crashes if it doesn't exist
+exports.tours = {};
+
+// YouTube room webhook
+exports.youtubehook = "";
 
 /*
  * Mail
