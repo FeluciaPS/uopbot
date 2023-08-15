928f12ee303ec4d48f02f0638a1628fb5ac4bee7
FeluciaPS <TheMezStrikes@gmail.com>
2023-08-15 23:33:07 +0200

    aaaa

--- commands/2v2.js
+++ commands/2v2.js
@@ -10,37 +10,24 @@ module.exports = {
         gen8: function (room, user, args) {
             if (!Utils.canMakeTour(room, user)) return;
             Utils.checkGenerator(room, "gen8doublesou", args, "[Gen 8] 2v2 Doubles");
-
-            let bans = `Calyrex-Ice, Calyrex-Shadow, Eternatus, Jirachi, Kyurem-White, Lunala, Magearna, Marshadow, Melmetal, Mewtwo, Necrozma-Dusk Mane, Necrozma-Dawn Wings, Reshiram, Solgaleo, Tornadus, Urshifu-Rapid-Strike, Urshifu-Single-Strike, Whimsicott, Zacian, Zacian-Crowned, Zamazenta, Zamazenta-Crowned, Zekrom, Ally Switch, Focus Sash, Perish Song`;
-            let rules = `Standard Doubles, Accuracy Moves Clause, Swagger Clause, Sleep Clause Mod, Team Preview`;
-            bans = bans.replace(/\,\s/g, ', -');
-            room.send(`/tour rules ${rules}, -${bans}, ${moncountrule}`);
+            room.send(`/tour rules +Kartana, -Cottonee, -Whimsicott, -Urshifu-Rapid-Strike, -Tornadus-Base, -Focus Sash, -Ally Switch, -Final Gambit, ${moncountrule}`);
         },
         gen7: function (room, user, args) {
             if (!Utils.canMakeTour(room, user)) return;
             Utils.checkGenerator(room, "gen7doublesou", args, "[Gen 7] 2v2 Doubles");
-
-            let bans = `Ally Switch, Arceus, Dialga, Giratina, Groudon, Ho-Oh, Jirachi, Kangaskhan-Mega, Kyogre, Kyurem-White, Lugia, Lunala, Magearna, Marshadow, Mewtwo, Necrozma-Dawn-Wings, Necrozma-Dusk-Mane, Palkia, Rayquaza, Reshiram, Salamence-Mega, Solgaleo, Tapu Lele, Xerneas, Yveltal, Zekrom, Power Construct, Focus Sash, Dark Void, Final Gambit, Perish Song`;
-            let rules = `Pokemon, Standard Doubles, Accuracy Moves Clause, Swagger Clause, Z-Move Clause, Sleep Clause Mod, Team Preview`;
-            bans = bans.replace(/\,\s/g, ', -');
-            room.send(`/tour rules ${rules}, -${bans}, ${moncountrule}`);
+            room.send(`/tour rules Accuracy Moves Clause, Z-Move Clause, +Gengar-Mega, -Kangaskhan-Mega, -Salamence-Mega, -Final Gambit, -Focus Sash, -Tapu Lele, +Snorlax, -Perish Song, ${moncountrule}`);
         },
         oras: "gen6",
         gen6: function (room, user, args) {
             if (!Utils.canMakeTour(room, user)) return;
             Utils.checkGenerator(room, "gen6doublesou", args, "[Gen 6] 2v2 Doubles");
-
-            let bans = `Mewtwo, Lugia, Ho-Oh, Kyogre, Groudon, Rayquaza, Jirachi, Dialga, Palkia, Giratina, Giratina-Origin, Arceus, Reshiram, Zekrom, Kyurem-White, Xerneas, Yveltal, Kangaskhan-Mega, Salamence-Mega, Dark Void, Perish Song, Soul Dew, Focus Sash`;
-            let rules = `Obtainable, Species Clause, OHKO Clause, Accuracy Moves Clause, Evasion Moves Clause, Moody Clause, Swagger Clause, Endless Battle Clause, HP Percentage Mod, Cancel Mod, Team Preview`;
-            bans = bans.replace(/\,\s/g, ', -');
-            room.send(`/tour rules ${rules}, -${bans}, ${moncountrule}`);
+            room.send(`/tour rules Accuracy Moves Clause, -Kangaskhan-Mega, -Perish Song,-Focus Sash, ${moncountrule}`);
         },
         bw: "gen5",
         gen5: function (room, user, args) {
             if (!Utils.canMakeTour(room, user)) return;
             Utils.checkGenerator(room, "gen5doublesou", args, "[Gen 5] 2v2 Doubles");
-            let bans = `Accuracy Moves Clause, -Focus Sash, -Kingdra, -Final Gambit, -Perish Song, -Ally Switch, Team Preview`;
-            room.send(`/tour rules ${bans}, ${moncountrule}`);        
+            room.send(`/tour rules Accuracy Moves Clause, -Perish Song,-Focus Sash, -Final Gambit, -Kingdra, ${moncountrule}`);        
         },
         dp: "gen4",
         gen4: function (room, user, args) {
