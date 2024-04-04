51f97c8084c6ed1730a0f2abff83e44b3582065a
FeluciaPS <TheMezStrikes@gmail.com>
2024-04-04 20:43:03 +0200

    Move Deutsche tours

--- commands/officials.js
+++ commands/officials.js
@@ -147,7 +147,7 @@ global.Officials = {
         },
     },
     deutsche: {
-        times: [19],
+        times: [18],
         randomformats: {
             gen9randombattle: 1,            gen8randombattle: 1,
             gen7randombattle: 1,            gen6randombattle: 1,
